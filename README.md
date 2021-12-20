2b803c0c5c4f31d461207e586a5a03e81e8db4b5
Lida Lucia Lopez <93487954+Lilop243@users.noreply.github.com>
2021-12-20 17:33:08 -0500

    Consumo-API-Rick-and-Morty

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # Consumo-API-Rick-and-Morty
 Consumo API Rick and Morty
+
+
+Entrego ejercicio de consumo API
+me guie con las clases grabadas y videos de youtube
---
+++ script/promise.js
@@ -0,0 +1,32 @@
+const API = 'https://rickandmortyapi.com/api/character/';
+
+const getData = (apiURL) => {
+    return fetch(apiURL)
+    .then(response => response.json())
+    .then(json => { printData(json) })
+    .catch(error => { console.error('Error: ', error)})
+ 
+}
+
+const printData = (data) => {
+    let html = '';
+    data.results.forEach(c => {
+          html += '<div class="col">';
+          html += '<div class="card" style="width: 13rem;">'
+          html += `<img src="${c.image}" class="card-img-top" alt="">`
+
+          html += '<div class="card-body">'
+          html += `<h5 class="card-title">${c.name}</h5>`
+
+
+          html += `<p class="card-text">Genero: ${c.gender}</p>`
+          html += `<p class="card-text">Especie: ${c.species}</p>`
+          html += '</div>'
+          html += '</div>'
+          html += '</div>'
+        
+    });
+    document.getElementById('infoCharacters').innerHTML = html
+}
+
+getData(API)
